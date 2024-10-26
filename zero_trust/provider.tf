07947db96ec3f44582282189a105c1a09957fde6
FSemti Dev <fabrice@fabricesemti.com>
2024-10-26 20:30:09 +0100

    refactor: :zap: separate files

--- zero_trust/provider.tf
+++ zero_trust/provider.tf
@@ -1,13 +1,11 @@
-
 terraform {
   required_providers {
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
       version = "~> 4"
     }
   }
 }
-
 provider "cloudflare" {
   api_token = var.cf_api_token
 }
--- zero_trust/tunnels.tf
+++ zero_trust/tunnels.tf
@@ -1,84 +1,11 @@
 # Define the tunnel resource
 #? Import it with "terraform import cloudflare_zero_trust_tunnel_cloudflared.existing_tunnel ${TF_VAR_cf_account_id}/ea8ef032-4e1b-408d-ba6d-abd101a6d54c"
 #? $ terraform import cloudflare_zero_trust_tunnel_cloudflared.example <cf_account_id>/<tunnel_id>
-# Define locals for hostname, domain, and port
-locals {
-  domains = [
-    {
-      name     = "casa"
-      hostname = "casa.fabricesemti.dev"
-      port     = 8080
-    },
-    {
-      name     = "jelly"
-      hostname = "jelly.fabricesemti.dev"
-      port     = 8097
-    },
-    {
-      name     = "nextcloud"
-      hostname = "nextcloud.fabricesemti.dev"
-      port     = 10081
-    },
-    {
-      name     = "plex"
-      hostname = "plex.fabricesemti.dev"
-      port     = 32400
-    },
-    {
-      name     = "portainer"
-      hostname = "portainer.fabricesemti.dev"
-      port     = 9000
-    },
-    {
-      name     = "prowlarr"
-      hostname = "prowlarr.fabricesemti.dev"
-      port     = 9696
-    },
-    {
-      name     = "sonarr"
-      hostname = "sonarr.fabricesemti.dev"
-      port     = 8989
-    },
-    {
-      name     = "transmission"
-      hostname = "transmission.fabricesemti.dev"
-      port     = 9091
-    },
-  ]
-}
 # Define the tunnel resource and import it
 resource "cloudflare_zero_trust_tunnel_cloudflared" "existing_tunnel" {
   account_id = var.cf_account_id
   name       = "docker-tunnel-new"
   secret     = var.tunnel_secret_docker
-}
-# Add DNS records for each domain specified in locals
-resource "cloudflare_record" "dns_records" {
-  for_each = { for domain in local.domains : domain.name => domain }
-  zone_id = var.cf_zone_id
-  name    = each.value.name
-  content = "${cloudflare_zero_trust_tunnel_cloudflared.existing_tunnel.id}.cfargotunnel.com"
-  type    = "CNAME"
-  proxied = true
-}
-# Configure tunnel ingress rules dynamically based on locals
-resource "cloudflare_zero_trust_tunnel_cloudflared_config" "tunnel_config" {
-  account_id = var.cf_account_id
-  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.existing_tunnel.id
-  config {
-    # Dynamic ingress rules from locals
-    dynamic "ingress_rule" {
-      for_each = local.domains
-      content {
-        hostname = ingress_rule.value.hostname
-        service  = "http://localhost:${ingress_rule.value.port}"
-      }
-    }
-    # Default catch-all rule
-    ingress_rule {
-      service = "http_status:404"
-    }
-  }
 }
 # Outputs for Cloudflare configuration
 output "tunnel_id" {
