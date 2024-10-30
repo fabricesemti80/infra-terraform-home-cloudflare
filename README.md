6714459a704cad592cb458af5d29751b42854c6e
FSemti Dev <fabrice@fabricesemti.com>
2024-10-30 08:33:24 +0000

    build: :fire: moved state backend
    In order to allow this project to be run remotely - ie. from Jenkins - I am moving the backend to TF Cloud!
    
    If you want to keep this local, remove / comment out the  `backend "remote" {}` block !

---
+++ README.md
@@ -0,0 +1,6 @@
+#
+
+## Update - 30/10/2024
+
+- Moved state to [Terraform Cloud back end](https://app.terraform.io/app/homelab-fsemti/workspaces/homelab-fsemti)
+- note: variables added as env vars on the workspace with `TF_VAR_` prefix
--- provider.tf
+++ provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 4"
     }
   }
+
+	backend "remote" {
+		organization = "homelab-fsemti" # org name from step 2.
+		workspaces {
+			name = "homelab-fsemti" # name for your app's state.
+		}
+	}  
+  
 }
 provider "cloudflare" {
   api_token = var.cf_api_token
