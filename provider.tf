2add3f1aac69dca35ace3683ea837559f614a445
Fabrice Semti <fabrice@fabricesemti.com>
2025-03-21 17:14:00 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,21 +6,13 @@ terraform {
     }
   }
 
-  # backend "remote" {
-  #   organization = "homelab-fsemti"
-  #   workspaces {
-  #     name = "homelab-fsemti"
-  #   }
-  # }
+  backend "remote" {
+    organization = "homelab-fsemti"
+    workspaces {
+      name = "tf-cloudflare"
+    }
+  }
 }
-
-# backend "remote" {
-# 	organization = "homelab-fsemti" # org name from step 2.
-# 	workspaces {
-# 		name = "homelab-fsemti" # name for your app's state.
-# 	}
-# }  
-
 
 provider "cloudflare" {
   api_token = var.cf_api_token
