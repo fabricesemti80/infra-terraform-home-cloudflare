5dc6ed55fc5cb97b5d15373b24c24bd5131744c1
Fabrice Semti <fabrice@fabricesemti.com>
2025-03-22 11:46:30 +0000

    typo fix

--- variables.tf
+++ variables.tf
@@ -5,31 +5,31 @@
 variable "cf_account_id" {
   description = "Cloudflare account ID"
   type        = string
-  sensitive = false
+  sensitive   = false
 }
 
 variable "cf_zone_id" {
   description = "Cloudflare zone ID"
   type        = string
-  sensitive = false
+  sensitive   = false
 }
 
 variable "cf_domain" {
   description = "Base domain for Cloudflare"
   type        = string
-  sensitive = false
+  sensitive   = false
 }
 
 variable "cf_api_token" {
   description = "Cloudflare API token"
   type        = string
-  sensitive = tru
+  sensitive   = true
 }
 
 variable "tunnel_secret" {
   description = "Secret for the Cloudflare tunnel"
   type        = string
-  sensitive = true
+  sensitive   = true
 }
 
 #------------------------------------------------------------------------------
@@ -40,14 +40,14 @@ variable "tunnel_name" {
   description = "Name of the Cloudflare tunnel"
   type        = string
   default     = "terraformed-docker-tunnel"
-  sensitive = false
+  sensitive   = false
 }
 
 variable "credentials_file_path" {
   description = "Path to store tunnel credentials files"
   type        = string
   default     = "~/.cloudflared"
-  sensitive = false
+  sensitive   = false
 }
 
 #------------------------------------------------------------------------------
