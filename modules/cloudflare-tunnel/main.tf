99b981e0694cc0010a40dfe19da515c9a8613a3f
Fabrice Semti <fabrice@fabricesemti.com>
2025-09-27 21:38:54 +0100

    refactor(cloudflare-tunnel): adjust file paths to move tunnel config files up two directories

--- modules/cloudflare-tunnel/main.tf
+++ modules/cloudflare-tunnel/main.tf
@@ -31,7 +31,7 @@ resource "cloudflare_zero_trust_tunnel_cloudflared" "tunnel" {
 
 # Write tunnel credentials to local JSON file for cloudflared
 resource "local_file" "tunnel_token" {
-  filename = "${path.module}/${var.config_dir}/${var.tunnel_name}-token.json"
+  filename = "${path.module}/../../${var.config_dir}/${var.tunnel_name}-token.json"
   content  = jsonencode({
     name          = cloudflare_zero_trust_tunnel_cloudflared.tunnel.name
     tunnel_secret = random_password.tunnel_secret.result
@@ -46,17 +46,17 @@ resource "local_file" "cloudflared_token" {
     s = base64encode(random_password.tunnel_secret.result)
     t = cloudflare_zero_trust_tunnel_cloudflared.tunnel.id
   }))
-  filename = "${path.module}/${var.config_dir}/${var.tunnel_name}-cloudflared-token.txt"
+  filename = "${path.module}/../../${var.config_dir}/${var.tunnel_name}-cloudflared-token.txt"
 }
 
 # Generate tunnel configuration file with ingress rules
 resource "local_file" "tunnel_config" {
   content = yamlencode({
     tunnel          = cloudflare_zero_trust_tunnel_cloudflared.tunnel.id
-    credentials-file = "${path.module}/${var.config_dir}/${var.tunnel_name}-token.json"
+    credentials-file = "${path.module}/../../${var.config_dir}/${var.tunnel_name}-token.json"
     ingress         = var.ingress_rules
   })
-  filename = "${path.module}/${var.config_dir}/${var.tunnel_name}-config.yaml"
+  filename = "${path.module}/../../${var.config_dir}/${var.tunnel_name}-config.yaml"
 }
 
 # Tunnel configuration in Cloudflare
